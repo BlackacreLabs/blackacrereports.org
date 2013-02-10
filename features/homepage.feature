c48bca8d9dfb59f759c6279d8078269f55c53222
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-02-10 11:14:11 -0600

    removed cucumber features

--- features/homepage.feature
+++
@@ -1,5 +0,0 @@
-Feature: Homepage
-
-  Scenario: Visit to the homepage
-    When I go to the homepage
-    Then I should see "Blackacre Reports"
--- features/step_definitions/web_steps.rb
+++
@@ -1,7 +0,0 @@
-When /^I go to the homepage$/ do
-    visit '/'
-end
-
-Then /^I should see "(.*?)"$/ do |text|
-  page.should have_content(text)
-end
